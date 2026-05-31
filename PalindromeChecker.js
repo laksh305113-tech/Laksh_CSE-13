74164b6a52315956b6f4f6315b9869637d092575
laksh305113-tech <laksh305113@gmail.com>
2026-05-31 17:51:06 +0530

    Create PalindromeChecker.js

---
+++ PalindromeChecker.js
@@ -0,0 +1,11 @@
+// Palindrome Checker
+
+function isPalindrome(word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+
+// Example Test Cases
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("racecar")); // true
