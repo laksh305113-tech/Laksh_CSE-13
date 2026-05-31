6c8cbec2b54333fb1e86cd78ed4b6f6a85d622d2
laksh305113-tech <laksh305113@gmail.com>
2026-05-31 17:51:41 +0530

    Create ReverseString.js

---
+++ ReverseString.js
@@ -0,0 +1,10 @@
+// Reverse a String in JavaScript
+
+function reverseString(str) {
+    return str.split('').reverse().join('');
+}
+
+// Example Test Cases
+console.log(reverseString("hello"));      
+console.log(reverseString("javascript"));
+console.log(reverseString("OpenAI"));
